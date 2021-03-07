18b055c7342469bc93eae6b2862eb1762e028f9c
yaniv dadon <y2.71828@gmail.com>
2021-03-08 00:03:27 +0200

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print ("Hello World")
+import os
+import sys
+
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    message = 'Yaniv -Great!\n'
+    version = 'Python %s\n' % sys.version.split()[0]
+    response = '\n'.join([message, version])
+    return [response.encode()]
