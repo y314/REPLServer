c98eb6abfa73c72a1521010ce99a66dcdb8260de
yaniv dadon <y2.71828@gmail.com>
2021-03-15 12:54:52 +0200

    def initserver added

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ from flask import Flask,request,jsonify
 import os
 import time
 
+def initserver():
+    #init server
+    app=Flask(__name__)
+    basic={'yaniv':'hi'}
+    #run server
+    #if __name__ == '__main__':
+    app.run(debug=True)
 
-#init server
-app=Flask(__name__)
-basic={'yaniv':'hi'}
-#run server
-#if __name__ == '__main__':
-app.run(debug=True)
 
-
-@app.route ("/", methods=['GET'])
-def index():
-    return jsonify(basic)
+    @app.route ("/", methods=['GET'])
+    def index():
+        return jsonify(basic)
 
 
 
