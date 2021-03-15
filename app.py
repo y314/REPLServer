00f0ca51e191103267ea19b4c6cfa84c532850d4
yaniv dadon <y2.71828@gmail.com>
2021-03-15 12:31:52 +0200

    remover init function

--- app.py
+++ app.py
@@ -2,17 +2,18 @@ from flask import Flask,request,json
 import os
 import time
 
-#init server
-app=Flask(__name__)
-basic={'yaniv':'hi'}
-#run server
-if __name__ == '__main__':
-    app.run(debug=True)
+def init():
+    #init server
+    app=Flask(__name__)
+    basic={'yaniv':'hi'}
+    #run server
+    if __name__ == '__main__':
+        app.run(debug=True)
 
 
-@app.route ("/", methods=['GET'])
-def index():
-    return json.dumps(basic)
+    @app.route ("/", methods=['GET'])
+    def index():
+        return json.dumps(basic)
 
 
 
