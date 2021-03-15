0a0a8b01797fd03ad16304e5e98e9817a6b599e1
yaniv dadon <y2.71828@gmail.com>
2021-03-15 13:03:22 +0200

    init server variables fixed

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask,request,jsonify
 import os
-import time
-
 def initserver():
     #init server
     app=Flask(__name__)
@@ -9,11 +7,22 @@ def initserver():
     #run server
     #if __name__ == '__main__':
     app.run(debug=True)
-
+    msg('Application initiate OK')
 
     @app.route ("/", methods=['GET'])
     def index():
         return jsonify(basic)
+
+def msg(content):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    #message = 'Yaniv - Pass wsgi answerd!\n Now you shoud get the app.py\n'
+    message='Got MSG\n' + content
+    #version = 'Python %s\n' % sys.version.split()[0]
+    version='3'
+    response = '\n'.join([message, version])
+    return [response.encode()]
+
+
 
 
 
