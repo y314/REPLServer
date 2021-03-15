560e5aa4071402f736207e50837f2289a9f163df
yaniv dadon <y2.71828@gmail.com>
2021-03-15 13:21:01 +0200

    back to simple

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
 from flask import Flask,request,jsonify
-import os
+#init server
+app=Flask(__name__)
+basic={'yaniv':'hi'}
+#run server
+#if __name__ == '__main__':
+app.run(debug=True)
+#msg('Application initiate OK')
 
-def initserver():
-    #init server
-    app=Flask(__name__)
-    basic={'yaniv':'hi'}
-    #run server
-    if __name__ == '__main__':
-        app.run(debug=True)
-    #msg('Application initiate OK')
+@app.route ("/", methods=['GET'])
+def index():
+    return '<H1>Hi Yaniv</H1>'#{'hello':'Yaniv'}
 
-    @app.route ("/", methods=['GET'])
-    def index():
-        return {'hello':'Yaniv'}
-
-
-initserver()
